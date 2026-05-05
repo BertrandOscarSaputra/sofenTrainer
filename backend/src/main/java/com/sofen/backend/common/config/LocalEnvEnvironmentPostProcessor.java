25726c613e9248702a15a1fa79907b89bf58d333
BertrandOscarSaputra <bertrandosc@gmail.com>
2026-05-05 20:29:06 +0800

    fix: environment variable renew(deprecated)

--- backend/src/main/java/com/sofen/backend/common/config/LocalEnvEnvironmentPostProcessor.java
+++ backend/src/main/java/com/sofen/backend/common/config/LocalEnvEnvironmentPostProcessor.java
@@ -7,7 +7,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
